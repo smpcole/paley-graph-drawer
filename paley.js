9e9e9cc907d9aead3d4cec6ae20893b8f5bd0b2b
Sam Cole <scole3@uic.edu>
2013-08-21 12:52:40 -0500

    Wrap Paley functionality in newPaley() function
    Returns an object containing p, a reference to the canvas, and the draw() function

--- paley.js
+++ paley.js
@@ -1,30 +1,38 @@
-// Assume there is an object paley which contains the following:
+// Return a new Paley drawer object with the following fields:
 // -- p: a prime number === 1 mod 4
 // -- canvas: an HTML 5 canvas object
+// -- draw(): function to draw the Paley graph of order p on the canvas
+var newPaley = function(p, canvas) {
+	var paley = {};
+	paley.p = p;
+	paley.canvas = canvas;
 
-paley.draw = function() {
-	
-	var getVertex = function(i) {
+	paley.getVertex = function(i) {
 		// These are also the coordinates of the center of the canvas
-		var xRad = paley.canvas.width / 2;
-		var yRad = paley.canvas.height / 2;
+		var xRad = this.canvas.width / 2;
+		var yRad = this.canvas.height / 2;
 		var angle = i * 2 * Math.PI / this.p;
 		return {x: xRad + xRad * Math.cos(angle), y: yRad + yRad * Math.sin(angle)};
 	};
 
-	var ctx = paley.canvas.getContext("2d");
-	paley.ctx.clearRect(paley.canvas.width, paley.canvas.height);
-	for(var i = 0; i < this.p; i++) {
-		var iCoords = getVertex(i);
-		ctx.moveTo(iCoords.x, iCoords.y);
-		for(var residue = 1; j <= (this.p - 1) / 2; j++) {
-			var j = (i + residue * residue) % this.p;
-			if(j > i) {
-				var jCoords = getVertex(j);
-				ctx.lineTo(jCoords.x, jCoords.y);
-				ctx.stroke();
-				ctx.moveTo(iCoords.x, iCoords.y);
+	paley.draw = function() {
+
+		var ctx = this.canvas.getContext("2d");
+		ctx.clearRect(this.canvas.width, this.canvas.height);
+		for(var i = 0; i < this.p; i++) {
+			var iCoords = this.getVertex(i);
+			ctx.moveTo(iCoords.x, iCoords.y);
+			for(var residue = 1; j <= (this.p - 1) / 2; j++) {
+				var j = (i + residue * residue) % this.p;
+				if(j > i) {
+					var jCoords = this.getVertex(j);
+					ctx.lineTo(jCoords.x, jCoords.y);
+					ctx.stroke();
+					ctx.moveTo(iCoords.x, iCoords.y);
+				}
 			}
 		}
-	}
+	};
+
+	return paley;
 };
